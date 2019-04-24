8ca7d0d944c1e2bf8c50929203833addd24eeeb5
Sam <thesamreaves@gmail.com>
2019-04-23 23:51:23 -0400

    Initial Commit

---
+++ SecretNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecretNotes
+//
+//  Created by Sam Reaves on 4/23/19.
+//  Copyright © 2019 Sam Reaves Digital. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
